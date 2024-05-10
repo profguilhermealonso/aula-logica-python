c6bb9893a8f75bb42f8a36e4a6e0ac9354a60c11
Prof Guilherme <professorguilhermealonso@gmail.com>
2024-05-10 16:19:25 -0300

    Ex01 e 02 em python

--- ex01.py
+++ ex01.py
@@ -1,1 +1,14 @@
-print("github funcionado")
+# Calculadora de média - valor declarado na variável
+
+nota1 = float(input("Digite a nota 1: "))
+nota2 = float(input("Digite a nota 2: "))
+
+media = (nota1 + nota2) / 2
+
+print("A média é: " + str(media))
+
+# verifica se aluno foi APROVADO ou REPROVADO
+if(media >= 7):
+    print("O aluno foi aprovado. Média final foi: " + media)
+else:
+    print("O aluno foi reprovado. Média final foi: " + media)
---
+++ ex02.py
@@ -0,0 +1,8 @@
+# Verificador de Número Par ou Ímpar
+
+num = float(input("Digite um número: "))
+
+if(num % 2 == 0):
+    print("O número é PAR.")
+else:
+    print("O número é ÍMPAR")
