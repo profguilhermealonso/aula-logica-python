fd07626fa6fdee369656ed17622f4d09504e4872
Prof Guilherme <professorguilhermealonso@gmail.com>
2024-05-10 17:49:05 -0300

    add ex03 e 04 em python

---
+++ ex03.py
@@ -0,0 +1,8 @@
+# conversor de tempetura - Celsius para fahrenheit
+
+# solicita a temperatura ao usuário
+temp_celsius = float(input("Digite a temperatura: "))
+
+temp_fahrenheit = (temp_celsius * 9/5) + 32
+
+print("A temperatura é: " + str(temp_fahrenheit))
---
+++ ex04.py
@@ -0,0 +1,7 @@
+# Calculo de IMC
+
+peso = float(input("Digite seu peso: "))
+altura = float(input("Digite sua altura: "))
+
+imc = (peso / altura ** 2)
+print("Seu IMC é de: " + str(imc))
